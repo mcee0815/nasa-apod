bed7a15580f7165b67c4988ff2427c516d1ab3ff
mike <mikecee0815@gmail.com>
2021-03-16 22:42:22 -0400

    link to repo

--- src/social.js
+++ src/social.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Social = ({repo,url}) => {
     
     return(
-        <div style={{marginTop:'15px',width:'100px',margin:'0 auto',background:'orange',borderRadius:'5px'}}>
+        <div style={{marginTop:'15px',width:'75px',margin:'0 auto',background:'orange',borderRadius:'5px'}}>
             <p style={{textAlign:'center',fontWeight:'bolder',background:'orange'}}>
                 <a style={{textDecoration:'none'}} href = {url}>
                     {repo}
