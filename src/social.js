8dc1df0a3e75bf74f51bf168f02ea2443631c2c4
mike <mikecee0815@gmail.com>
2021-03-16 23:12:38 -0400

    changed fon weight

--- src/social.js
+++ src/social.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
 const Social = ({repo,url}) => {
     
     return(
-        <div style={{marginTop:'15px',width:'75px',margin:'0 auto',background:'orange',borderRadius:'5px'}}>
-            <p style={{textAlign:'center',fontWeight:'bolder',background:'orange'}}>
+        <div style={{marginTop:'15px',width:'75px',background:'orange',borderRadius:'5px'}}>
+            <p style={{textAlign:'center',fontWeight:'normal',background:'orange'}}>
                 <a style={{textDecoration:'none'}} href = {url}>
                     {repo}
                 </a>
